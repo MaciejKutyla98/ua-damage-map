47cb972b68702d32caa365675a184d47a76d2345
Tymek Zapała <tymzap@gmail.com>
2022-03-19 21:21:45 +0100

    Add Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Layout as AntLayout } from 'antd';
+import styles from './Layout.module.scss';
+
+export const Layout = ({children}) => {
+  return (
+    <AntLayout>
+      <AntLayout.Header className={styles.header}>
+        <h1 className={styles.heading}>UA Damage Map</h1>
+      </AntLayout.Header>
+      <AntLayout.Content>
+        {children}
+      </AntLayout.Content>
+    </AntLayout>
+  )
+};
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import {MapProvider} from "./hooks/mapHook";
 import {Map} from "./components/Map/Map";
+import { Layout } from './components/Layout/Layout';
+
 
 ReactDOM.render(
     <MapProvider>
+      <Layout>
         <Map />
+      </Layout>
     </MapProvider>,
     document.getElementById("root")
 );
