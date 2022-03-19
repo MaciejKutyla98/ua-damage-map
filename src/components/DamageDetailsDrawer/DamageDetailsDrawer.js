cade70d4cc69842ed7e101651aac92a74684496d
Tymek Zapała <tymzap@gmail.com>
2022-03-20 00:07:39 +0100

    Add Map drawer for desktop

--- src/components/DamageDetailsDrawer/DamageDetailsDrawer.js
+++ src/components/DamageDetailsDrawer/DamageDetailsDrawer.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import cn from 'classnames';
+
+import { useIsMobile } from '../../hooks/useIsMobile';
+
+import styles from './DamageDetailsDrawer.module.scss';
 
 import { Drawer } from 'antd';
 
 export const DamageDetailsDrawer = ({damageDetails, visible, onClose}) => {
+  const isMobile = useIsMobile();
+
+  console.log('is', isMobile);
+
   return (
-    <Drawer placement={'bottom'} visible={visible} onClose={onClose}>damage details drawer</Drawer>
+    <Drawer className={cn(styles.drawer, {[styles.drawerDesktop]: !isMobile})} placement={isMobile ? 'bottom' : 'left'} visible={visible} onClose={onClose}>damage details drawer</Drawer>
   )
 };
--- src/hooks/useIsMobile.js
+++ src/hooks/useIsMobile.js
@@ -1,5 +1,7 @@
 import { useWindowSize } from 'react-use';
 
 export const useIsMobile = () => {
+  const {width, height} = useWindowSize();
 
+  return width < 900;
 };
